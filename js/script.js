a794f879b2a70cbf770041519643d45ac958badf
lilylam81 <lilylam81@gmail.com>
2022-10-13 21:51:30 -1000

    updated personal framework

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // OVERLAY NAV MENU SHOW HIDE
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
 
    const mymenubutton = document.querySelector('.menu-button');
    const mysitenav = document.querySelector('.site-header .site-nav');
 
-   mymenubutton.onclick = function () {
+   mymenubutton.onclick = function() {
       if (mysitenav.getAttribute('data-navstate') === 'open') {
-          mysitenav.setAttribute('data-navstate', 'closed')
+          mysitenav.setAttribute('data-navstate', 'closed');
       } else {
-          mysitenav.setAttribute('data-navstate', 'open')
+          mysitenav.setAttribute('data-navstate', 'open');
       }
    };
 
